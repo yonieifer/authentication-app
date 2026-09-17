dd1d12657bf72adff5b0e3b1d75be367fe70bb82
Yonatan Eiferman <yonieifer@gmail.com>
2026-09-17 17:39:32 +0300

    add logout btn

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+    const navigate = useNavigate()
+    const onLogout = () => {
+        localStorage.removeItem("token")
+        navigate("/log-in")
+    }
     return (
         <>
             <nav>
                 <Link to="log-in">login</Link>
                 <Link to="/sign-up">sign up</Link>
-                <Link to="/log-out">logout</Link>
+                <button onClick={onLogout}>logout</button>
             </nav>
         </>
     );
