64933d20eae6c83638e6f4ec33f56a4641d311bf
Yonatan Eiferman <yonieifer@gmail.com>
2026-09-17 17:59:53 +0300

    end

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-import { Outlet, useNavigate , Navigate} from "react-router-dom";
+import { Outlet , Navigate} from "react-router-dom";
 import useVerify from "../hooks/useVerify";
 
 function ProtectedRoute() {
     const { error, loading, data } = useVerify();
-    const navigate = useNavigate();
-    if (!data) return <Navigate to="/"/>;
+    if (!data && !loading) return <Navigate to="/"/>;
     return <Outlet />;
 }
 
