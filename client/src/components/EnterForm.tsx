8ad5ae7ab26883e09aa545838921a5bf215867d5
Yonatan Eiferman <yonieifer@gmail.com>
2026-09-17 11:28:28 +0300

    add enter form component

---
+++ client/src/components/EnterForm.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+interface FormProps {
+    onSubmit: (username: string, email: string, password: string) => void;
+    action: string;
+}
+
+function EnterForm({ onSubmit, action }: FormProps) {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <>
+            <form>
+                <input
+                    type="text"
+                    placeholder="username"
+                    required
+                    onChange={(e) => setUsername(e.target.value)}
+                    value={username}
+                />
+                <input
+                    type="emil"
+                    placeholder="email"
+                    required
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+                <input
+                    type="text"
+                    placeholder="password"
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
+                <button onClick={() => onSubmit(username, email, password)}>
+                    {action.toUpperCase()}
+                </button>
+            </form>
+        </>
+    );
+}
+
+export default EnterForm;
